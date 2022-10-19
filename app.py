678a938d8a45572c7743857148394711077d8391
RutgerJTK <43109428+RutgerJTK@users.noreply.github.com>
2022-10-19 15:32:15 +0200

    Finished afvinktaak 1 Bi8a

--- app.py
+++ app.py
@@ -2,55 +2,61 @@ import os
 
 import pymysql
 from flask import Flask, render_template, request
-from Bio import Entrez
-from pymysql import Connect
-# from mysql import mysql.connector
+
 martin_folder = os.path.join('static', 'MartinCat.jpeg')
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def homepage():  # put application's code here
-
     return render_template('homepage.html')
 
 
 @app.route('/week_1', methods=['GET', 'POST'])
-def week_1():   # enter week 1 homepage
-    print("yoyo")
-    if request.method == 'POST':
-        query = request.form['query_1']
-        print(query)
-        print("*"*80)
-        # Entrez.email = 'rjtkemperman@gmail.com'
-        # data = Entrez.esearch(db="", term=query)
-        # record = Entrez.read(data)
-        # print(record)
-        db = pymysql.connect(host="ensembldb.ensembl.org",
-            user="anonymous",
-            password="",
-            database="homo_sapiens_core_95_38")
-        cursor = db.cursor()
-        cursor.execute("SHOW Tables")
-        cursor.execute('''SELECT * FROM gene where description like {'havana%'} limit 10''')
-        print(cursor.description)
-        myresults = cursor.fetchall()
-        for x in myresults:
-            print(*x)
-        cursor.close()
-        db.close()
+def week_1():  # enter week 1 homepage
+    try:
+        str_results = ""
+        str_list = []
+        if request.method == 'POST':
+            query = request.form['query_1']
+            print("*" * 80)
+            db = pymysql.connect(host="ensembldb.ensembl.org",
+                                 user="anonymous",
+                                 password="",
+                                 database="homo_sapiens_core_95_38")
+            cursor = db.cursor()
+            cursor.execute("SHOW Tables")
+            cursor.execute("""SELECT * FROM gene where description like %(p)s limit 10""",
+                           {"p": "%{}%".format(query)})
+            results = cursor.fetchall()
+            cursor.close()
+            db.close()
+            print(len(query), "and: ", len(results))
 
-    else:
-        print("")
-    return render_template('week1.html')
+            if len(query) and len(results) >= 1:
+                print(len(query), "and: ", len(results))
+                for i in results:
+                    str_results = (i[8] + ": " + i[9])
+                    str_list.append(str_results)
+                return render_template('week1.html', myresults=str_list, query=query)
+            else:
+                return render_template('week1.html',
+                                       noresults="No results were found - please search again \
+                                       you poor excuse for a human being ^-^")
+        else:
+            print("")
+        return render_template('week1.html')
+    except IOError:
+        print("Something went wrong")
+    except Exception as E:
+        print("Something went wrong::", "\n", E)
 
 
 @app.route('/week_2', methods=['GET', 'POST'])
-def week_2():   # enter week 1 homepage
+def week_2():  # enter week 1 homepage
     try:
         if request.method == 'post':
-            print("Wow!")
-
+            print("Entered week 2!")
             return render_template('week2.html')
         return render_template('week2.html')
     except Exception as E:
